9fd4abbb73c1dd97e5f77cc8c0b900ed8fef9e8a
Robustiano Aguirre Cambera <tianocambera18@gmail.com>
2025-12-18 01:30:02 -0300

    Actualizacion Hero

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // -----------------------------------------------------------------------
+  // IMPORTANTE:
+  // 1. Para trabajar en tu PC (npm run dev): Deja la línea de abajo COMENTADA (con //).
+  // 2. Para subir a GitHub (npm run deploy): Quítale las // para activarla.
+  // -----------------------------------------------------------------------
+  
+  base: "/TFI-HotelEnricar/",
 })
